4a89fc79bf854b3c8b55c2caf7e0df159f543212
Richard Hallam <richardhallam25@gmail.com>
2023-10-26 19:44:13 +0100

    test

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -4,7 +4,7 @@ const Artist = ({ image, name }) => {
   return (
     <div className='card'>
       <img src={image} className='img' alt='' />
-      <div className="h2">heading2</div>
+      <div className="h2">heading 2</div>
       <div className='h3'>{name}</div>
     </div>
   );
